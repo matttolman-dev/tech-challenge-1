70b1bd807616ddc33ecc6d390360ef06338e5358
Matt Tolman <mtolman@Matts-Mac-mini.local>
2024-03-03 21:44:14 -0700

    initial project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to loanpro-interview
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/loanpro_interview/core_test.clj
@@ -0,0 +1,7 @@
+(ns loanpro-interview.core-test
+  (:require [clojure.test :refer :all]
+            [loanpro-interview.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
